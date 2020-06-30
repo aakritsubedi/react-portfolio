fc4955462318c63a9afbcaa9f09cd1a1045d2e50
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-06-30 13:49:29 +0545

    New Build

---
+++ build/precache-manifest.cd6e40f6161608757a78ab684762113a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbbee58d968b523d21bf929194aa0ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "29ba7487f319096400fe",
+    "url": "/static/css/2.11f871ab.chunk.css"
+  },
+  {
+    "revision": "e197c206ab6374e43556",
+    "url": "/static/css/main.56c79105.chunk.css"
+  },
+  {
+    "revision": "29ba7487f319096400fe",
+    "url": "/static/js/2.3a22dfa5.chunk.js"
+  },
+  {
+    "revision": "e197c206ab6374e43556",
+    "url": "/static/js/main.f5b0d946.chunk.js"
+  },
+  {
+    "revision": "5855204be87568697cbf",
+    "url": "/static/js/runtime-main.529f7743.js"
+  }
+]);
